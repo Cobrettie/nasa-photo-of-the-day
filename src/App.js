89c1cefb00fa735c5c20a253665ccb12ff575bdf
Cobrettie <cobrettiegarner3@gmail.com>
2019-08-28 18:58:17 -0400

    install axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate
+  useEffect(() => {
+    document.title = `You clicked me ${count} times`
+  })
+
   return (
     <div className="App">
+      <h1 id="test">Hey</h1>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+
+      <p>You clicked me {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+
     </div>
   );
 }
