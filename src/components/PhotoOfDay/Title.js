c8bcaa0b5022b68e494b99d7224fd785b55c420d
Cobrettie <cobrettiegarner3@gmail.com>
2019-08-29 21:02:27 -0400

    add heading to Title.js, MVP complete

--- src/components/PhotoOfDay/Title.js
+++ src/components/PhotoOfDay/Title.js
@@ -4,6 +4,7 @@ function Title(props) {
     // console.log(props);
     return (
         <div>
+            <h1>NASA Photo Of The Day</h1>
             <h2>{props.title}</h2>
         </div>
     );
